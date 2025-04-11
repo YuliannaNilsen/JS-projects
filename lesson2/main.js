48460c244faa01987dd30f72ec6a2ad6a8a18d40
YuliannaNilsen <yulianna.nilsen856@gmail.com>
2025-04-12 00:46:27 +0300

    js lesson1

---
+++ lesson2/main.js
@@ -0,0 +1,36 @@
+// let ree;
+// ree= [122, 25, 256, -200, 2, true, 'fggf'];
+//
+// console.log(ree[0]);
+// console.log(ree[2]);
+// console.log(ree[4]);
+// console.log(ree.length);
+//
+// ree[0] = 'new new';
+// console.log(ree)
+//
+// ree[7] = 'repa';
+// console.log(ree)
+//
+// ree[ree.length] = 'frrfrws'
+// console.log(ree);
+// ree[ree.length] = 'gdgdgfer'
+// console.log(ree);
+
+let user1 = {
+    id: 1,
+    name: 'sasha',
+    age: 22,
+    skills : ['html', 'js', 'java'],
+    wife:  {
+        name: 'olya',
+        age: 28
+    }
+}
+
+console.log(user1);
+console.log(user1.name);
+console.log(user1['name']);
+console.log(user1.skills[2]);
+
+console.log(user1.wife.name)
