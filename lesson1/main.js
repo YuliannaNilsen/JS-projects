1a90c6c981d0ec0df71a53b12f252eb3da590b98
YuliannaNilsen <yulianna.nilsen856@gmail.com>
2025-04-11 12:36:32 +0300

    js

---
+++ lesson1/main.js
@@ -0,0 +1,89 @@
+// console.log('asdf');
+// var a = 'hello day';
+// let b;
+// b = 'hello world'
+
+// {
+//     let a = 'hello world'
+//     var b = 'happy day'
+// }
+// console.log(b);
+// console.log(a);
+
+
+// const PI = 3.14;
+// console.log(PI);
+// // PI = 123;
+//
+// let a= 'fgedgfd';
+// console.log(a);
+// a = 'dsfsdf';
+// console.log(a);
+
+// let a = 'hffw';
+// // a = '55';
+//
+// console.log(typeof a, a)
+//
+// let b = 100500;
+// console.log(typeof b, b);
+//
+// let c = true;
+// console.log(c)
+// let a = 'efsffsf'
+//
+// let b = `fdsfds`
+
+// let giro = 'hold'
+//
+// let intro = 'this call'
+//
+// let msg = giro +' '+ intro;
+// console.log(msg);
+//
+// let msg2 = `${giro} ${intro}`;
+// console.log(msg2);
+//
+
+
+// let a = 100500;
+//
+// console.log(a);
+//
+// let b = 3.1231424
+// console.log(b);
+//
+// let n1 = 200;
+// let n2 = 300;
+//
+// console.log(n1 * n2);
+
+
+
+// let a = true;
+//
+// console.log(10 === 20)
+// console.log(20 !== 20)
+// console.log(15 < 20)
+
+
+// console.log(typeof (10500 + ''));
+// console.log(100 + 200 + '');
+// console.log('' + 100 + 200);
+// console.log(+'10520002asd');
+// console.log(parseInt('522345452asd'))
+// console.log(parseInt('454545.855'))
+// console.log(parseFloat('454545.855'))
+
+let greeting= 'hello';
+console.log(greeting);
+
+let site1= 'owu';
+console.log(site1);
+
+let site2= 'com';
+console.log(site2);
+
+let site3= 'ua';
+console.log(site3)
+
