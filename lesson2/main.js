ef237a0732a2e1d464e9619e6412a4eb36a57b3d
YuliannaNilsen <yulianna.nilsen856@gmail.com>
2025-04-12 17:25:27 +0300

    js lesson2

--- lesson2/main.js
+++ lesson2/main.js
@@ -16,21 +16,100 @@
 // console.log(ree);
 // ree[ree.length] = 'gdgdgfer'
 // console.log(ree);
+//
+// let user1 = {
+//     id: 1,
+//     name: 'sasha',
+//     age: 22,
+//     skills : ['html', 'js', 'java'],
+//     wife:  {
+//         name: 'olya',
+//         age: 28
+//     }
+// }
+//
+// console.log(user1);
+// console.log(user1.name);
+// console.log(user1['name']);
+// console.log(user1.skills[2]);
+//
+// console.log(user1.wife.name)
+//
+// let users = [
+//     {skills: ['html', 'js', 'mongo'],id: 1, name:'vasya', age: 20, status: false},
+//     {skills: ['html', 'js', 'mysql'],id: 2, name:'petya', age: 31, status: true},
+//     {skills: ['html', 'js', 'mysql'],id: 3, name:'anna', age: 30, status: true},
+//     {skills: ['html', 'js', 'mongo'],id: 4, name:'olya', age: 28, status: false},
+//
+// ];
+//
+// let user0 = users[0];
+//
+// console.log(users);
+// console.log(users[0].name);
+// console.log(users[0]['age']);
+// console.log(user0.name);
+// console.log(users[2].skills[1])
+//
 
-let user1 = {
-    id: 1,
-    name: 'sasha',
-    age: 22,
-    skills : ['html', 'js', 'java'],
-    wife:  {
-        name: 'olya',
-        age: 28
-    }
-}
 
-console.log(user1);
-console.log(user1.name);
-console.log(user1['name']);
-console.log(user1.skills[2]);
+// let user= {
+//     id: 1,
+//     name: 'tanya',
+// }
+//
+// user.age = 31;
+// user['status'] = false;
+// console.log(user);
+//
+// delete user.id;
+// console.log(user);
+//
 
-console.log(user1.wife.name)
+// let color = 'red';
+// if (color === 'red') {
+//     console.log('stop');
+// }
+// else {
+//     console.log('go!');
+// }
+
+
+// let age = +prompt('enter your age');
+// if (age >= 18) {
+//     console.log('adult');
+// }
+// else    {
+//     console.log('cartoon');
+// }
+
+// let color = prompt('enter color');
+// if (color === 'green') {
+//     console.log('goo');
+// } else if (color === 'yellow') {
+//     console.log('wait');
+// } else if (color === 'red') {
+//     console.log('stop');
+// } else {
+//     console.log('???????');
+// }
+//
+
+
+// let color = prompt('enter color');
+//
+// switch (color) {
+//     case 'green':
+//         console.log('go');
+//         break;
+//     case 'yellow':
+//         console.log('wait');
+//         break
+//     case 'red':
+//         console.log('stop');
+//         break
+//     default:
+//         console.log('?????');
+// }
+
+
